91854eea9f8b82798802f733d00ef2d2533d6d86
koitt03-A <koitt03-A@koitt03-A-PC>
2018-08-28 16:25:59 +0900

    remove maven

--- ict_erp/src/com/ict/erp/controller/DepartServlet.java
+++ ict_erp/src/com/ict/erp/controller/DepartServlet.java
@@ -3,7 +3,6 @@ package com.ict.erp.controller;
 import java.io.IOException;
 import java.sql.SQLException;
 
-import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
